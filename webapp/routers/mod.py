80f4ef12da23bed1d65fc59d6a43b772fdeb955d
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-03-14 17:34:56 +0000

    endpoint for muting players via webapp

--- webapp/routers/mod.py
+++ webapp/routers/mod.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 
 from ..internal import security
-from ..models import AutomodTrigger
+from ..models import AutomodTrigger, PlayerMute
 
 router = APIRouter(
     prefix="/mod",
@@ -52,3 +52,21 @@ async def remove_trigger_from_db(request: Request, payload: AutomodTrigger) -> l
     request.app.db.set_config_row(db_payload)
 
     return trigger_list
+
+
+@router.post("/mute")
+async def mute_player(request: Request, payload: PlayerMute) -> PlayerMute:
+    if not 0 <= payload.length < 999999:
+        raise HTTPException(status_code=422, detail="Length must be between 0 and 999999 inclusive.")
+
+    mute_data = f"MUTE={payload.target}~{payload.length}~{payload.reason}~{payload.is_ip}"
+
+    mute_action = {
+        "target": "game",
+        "action": "send_ws_message",
+        "payload": mute_data,
+        "source": "webapp",
+    }
+
+    request.app.p_q.put(mute_action)
+    return payload
