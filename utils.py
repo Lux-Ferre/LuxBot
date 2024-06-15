0331fd9acf1f93b9ac86270924bc11390f48d2f9
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-06-15 15:22:03 +0100

    removed obsolete pastebin method

--- utils.py
+++ utils.py
@@ -12,21 +12,6 @@ class RepeatTimer(Timer):
 
 class Utils:
     @staticmethod
-    def dump_to_pastebin(paste_string: str, expiry: str) -> str:
-        api_key = os.environ["PASTEBIN_API_KEY"]
-        url = "https://pastebin.com/api/api_post.php"
-        data = {
-            "api_dev_key": api_key,
-            "api_option": "paste",
-            "api_paste_code": paste_string,
-            "api_paste_expire_date": expiry
-        }
-
-        response = requests.post(url=url, data=data)
-
-        return response.text
-
-    @staticmethod
     def gen_send_action(target: str, reply_data: dict):
         send_action = None
 
