a0c184129ab7ada877cd63c9c58efde3da852e3c
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-09-23 14:46:46 +0100

    new message class to replace message/action dicts
    Should provide the same functionality with easier to read code.
    
    Similar to a linked list, the ``previous`` attribute maintains the message chain, allowing the data to be accessed if needed without adding unnecessary bloat and nesting within the payload.
    
    The ``get_first_message`` method will return the root message in the chain, this is especially useful for generating responses. Eg, the first message's source is likely the response's intended target.

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-import os
-import requests
-
+from typing import Self, Optional
+from datetime import datetime, timezone
 from threading import Timer
 
 
@@ -44,3 +43,59 @@ class Utils:
         }
 
         return mute_action
+
+
+class Message:
+    def __init__(self, target: str, action: str, payload: str, source: str, previous: Optional[Self] = None):
+        """
+        Object for transporting data and commands through the application.
+        :param target: The module which the Message should be sent to.
+        :param action: Which method withing the module the dispatcher should pass the message to.
+        :param payload: The data for use by the target's action method.
+        :param source: Where the Message has come from.
+        :param previous: The instance of Message which was processed to create this one, or None when it's the root.
+        """
+        self.target = target
+        self.action = action
+        self.payload = payload
+        self.source = source
+        self.timestamp = datetime.now(timezone.utc)
+        self.previous = previous
+
+    def __str__(self) -> str:
+        return (f"target: {self.target},\n" +
+                f"action: {self.action},\n" +
+                f"payload: {self.payload},\n" +
+                f"source: {self.source},\n" +
+                f"timestamp: {self.timestamp}\n" +
+                f"previous: {self.previous.__class__.__name__}")
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def new(self,
+            target: Optional[str] = None,
+            action: Optional[str] = None,
+            payload: Optional[str] = None,
+            source: Optional[str] = None
+            ) -> Self:
+        """
+        Returns a new Message using any args given, defaulting to self attributes where none are provided.
+        Self is assigned to 'previous' attribute.
+        :param target: The module which the Message should be sent to.
+        :param action: Which method withing the module the dispatcher should pass the message to.
+        :param payload: The data for use by the target's action method.
+        :param source: Where the Message has come from.
+        :return: New Message instance based on self.
+        """
+        target = target or self.target
+        action = action or self.action
+        payload = payload or self.payload
+        source = source or self.source
+
+        return Message(target, action, payload, source, self)
+
+    def get_first_message(self):
+        if self.previous is None:
+            return self
+        return self.previous.get_first_message()
